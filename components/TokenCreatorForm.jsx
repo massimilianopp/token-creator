7eed24973e47245ed9a213e15d40e9aa8d1cb831
Massimiliano <massimiliano.pappalardo2001@gmail.com>
2026-05-10 16:21:46 +0200

    title

--- components/TokenCreatorForm.jsx
+++ components/TokenCreatorForm.jsx
@@ -401,19 +401,8 @@ export default function TokenCreatorForm() {
   return (
     <div ref={formRef} style={{ display: "flex", flexDirection: "column", gap: 20, paddingBottom: "100px" }}>
       
-      {/* Header */}
+      {/* Progress bar */}
       <div style={{ marginBottom: 24 }}>
-        <h1 style={{
-          fontSize: 32,
-          fontWeight: 700,
-          color: "white",
-          fontFamily: "'Inter', sans-serif",
-          marginBottom: 8,
-        }}>
-          Create Token
-        </h1>
-        
-        {/* Progress bar */}
         <div style={{
           width: "100%",
           height: 2,
