ad2ce6e9ec95c5ceae1e1f3791004a0f41208de1
Massimiliano <massimiliano.pappalardo2001@gmail.com>
2026-07-14 07:29:05 +0200

    GA

--- app/layout.js
+++ app/layout.js
@@ -61,3 +61,39 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+
+import Script from 'next/script'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRB5BXQ2');
+          `}
+        </Script>
+      </head>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRB5BXQ2"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
+    </html>
+  )
+}
