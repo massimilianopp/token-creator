95f728b83ef3ad25d8329f8d8dc655a2e29bbaa5
Massimiliano <massimiliano.pappalardo2001@gmail.com>
2026-07-29 03:57:36 +0200

    dashboard

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -9,7 +9,7 @@ const NAV_ITEMS = [
   { href: "/", label: "Token", number: "01" },
   { href: "/vesting", label: "Vesting", number: "02" },
   { href: "/pool", label: "Pool", number: "03" },
-  { href: "/explore", label: "Explore", number: "04" },
+  { href: "/dashboard", label: "Dashboard", number: "04" },
   { href: "/help", label: "Help", number: "?" },
 ];
 
